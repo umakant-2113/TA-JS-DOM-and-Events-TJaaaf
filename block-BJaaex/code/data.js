3fb2f659a7b36f2be280c6033aefa0a37b18b22e
umakant-2113 <lodhiumakant800@gmail.com>
2022-02-24 12:09:40 +0530

    assignment memory game

---
+++ block-BJaaex/code/data.js
@@ -0,0 +1,51 @@
+let memoryGameData = [
+    {
+        name : "superMario",
+        img : "https://img.icons8.com/doodle/48/000000/super-mario.png",
+    },
+    {
+        name : "ironMan",
+        img : "https://img.icons8.com/doodle/48/000000/iron-man.png",
+    },
+    {
+        name : "superMan",
+        img : "https://img.icons8.com/office/16/000000/superman.png",
+    },
+    {
+        name : "spiderMan",
+        img : "https://img.icons8.com/office/16/000000/spiderman-head--v1.png",
+    },
+    {
+        name : "walterWhite",
+        img : "https://img.icons8.com/doodle/48/000000/walter-white.png",
+    },
+    {
+        name : "stanMarsh",
+        img : "https://img.icons8.com/doodle/48/000000/walter-white.png",
+    },
+    {
+        name : "hackerMask",
+        img : "https://img.icons8.com/doodle/48/000000/anonymous-mask.png",
+    },
+    {
+        name : "greyIcon",
+        img : "https://img.icons8.com/doodle/50/000000/grey.png",
+    },
+    {
+        name : "EricCartMan",
+        img : "https://img.icons8.com/doodle/50/000000/eric-cartman.png",
+    },
+    {
+        name : "homerSimson",
+        img : "https://img.icons8.com/doodle/50/000000/homer-simpson.png",
+    },
+    {
+        name : "jakeDog",
+        img : "https://img.icons8.com/doodle/50/000000/jake.png",
+    },
+    {
+        name : "heartTeddy",
+        img : "https://img.icons8.com/doodle/50/000000/bt21-tata.png",
+    }
+
+];
